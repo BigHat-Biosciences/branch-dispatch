7f0cc1f5679d93e93ca6265ef50c21d4b16ca834
Jeremy Hert <jhert@bighatbio.com>
2020-08-18 17:13:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+/**
+ * This script is launched after master was pushed. We should wait until VERSION is available in pypi, then 
+ * trigger re-runs of all live branches. Those re-runs should bump versions in the other branches.
+ */
+
+async function run() {
+  try {
+    const token = core.getInput('repo-token');
+    const targetWorkflow = core.getInput('target-workflow');
+
+    const repoName = github.context.payload.repository.full_name;
+    core.info(`Running under ${repoName}`);
+    const [owner, repo] = repoName.split('/');
+
+    const octokit = github.getOctokit(token);
+    const { data: branches } = await octokit.repos.listBranches({
+      owner,
+      repo,
+    });
+
+    branches.forEach(async b => {
+      if (b.name !== 'master') {
+        core.info(`Running ${targetWorkflow} for ${b.name}`);
+        try {
+          const resp = await octokit.actions.createWorkflowDispatch({
+            owner,
+            repo,
+            workflow_id: targetWorkflow,
+            ref: b.name,
+          });
+        } catch (error) {
+          core.setFailed(JSON.stringify(error));
+        }
+      }
+    })
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
